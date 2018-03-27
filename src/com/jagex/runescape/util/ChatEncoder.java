ad1640889f0774b71ad13e502a20039eca61ae05
Dope Head <30884817+Dope-Head@users.noreply.github.com>
2018-03-27 11:12:22 -0500

    ChatEncoder: Refactored Field messageBuffer

--- src/com/jagex/runescape/util/ChatEncoder.java
+++ src/com/jagex/runescape/util/ChatEncoder.java
@@ -5,7 +5,7 @@ import com.jagex.runescape.net.Buffer;
 public class ChatEncoder {
 
 	private static char[] message = new char[100];
-	public static Buffer messageBuffer = new Buffer(new byte[100]);
+	private static Buffer messageBuffer = new Buffer(new byte[100]);
 	public static char VALID_CHARACTERS[] = {' ', 'e', 't', 'a', 'o', 'i', 'h', 'n', 's', 'r', 'd', 'l', 'u', 'm', 'w',
 			'c', 'y', 'f', 'g', 'p', 'b', 'v', 'k', 'x', 'j', 'q', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8',
 			'9', ' ', '!', '?', '.', ',', ':', ';', '(', ')', '-', '&', '*', '\\', '\'', '@', '#', '+', '=', '\243',
